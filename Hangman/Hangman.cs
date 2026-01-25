6a4239de34800b3bdcec44eeb50ddca463a3d1ea
Niaz Baharudeen <niazbaharudeen@gmail.com>
2026-01-25 16:13:01 -0500

    Created a constructor for Hangman that takes in a filepath for the input file and populates a word bank property with words from said file.

--- Hangman/Hangman.cs
+++ Hangman/Hangman.cs
@@ -1,9 +1,30 @@
-﻿namespace HangmanNS
+﻿using System.IO;
+
+namespace HangmanNS
 {
 
     public class Hangman
     {
-        public Hangman() { }
+        public List<string> WordBank { get; } = new List<string>();
+        public Hangman(string filePath) { 
+            StreamReader sr = new StreamReader(filePath);
+
+            var line = sr.ReadLine();
+
+            while(line != null)
+            {
+                WordBank.Add(line);
+                line = sr.ReadLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Hangman hangman = new Hangman("HangmanWords.txt");
+            hangman.WordBank.ForEach(Console.WriteLine);
+            Console.ReadLine();
+        }
+
     }
 
 }
